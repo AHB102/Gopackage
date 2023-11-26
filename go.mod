1ec6b917c440cefb3e2428439362ea443d49db4b
Archit H Barve <architbarve102@gmail.com>
2023-11-26 19:55:15 +0530

    First Commit for package

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/AHB102/Gopackage
+
+go 1.20
---
+++ main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"log"
+
+	"github.com/AHB102/Gopackage/mylogger"
+)
+
+func main() {
+	log.Printf("Hello from the main.go")
+	mylogger.Printfunc()
+}
